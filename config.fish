c0997e8cd0bea90d40643a8b90a75bb4f814914a
Batu Ergun <ergunbatuhan@icloud.com>
2026-04-07 20:45:57 +0200

    apple system colors

--- config.fish
+++ config.fish
@@ -11,22 +11,22 @@ set -gx EDITOR nvim
 # FZF
 set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
 
-# Fish colors (Gruvbox)
-set -g fish_color_normal      ebdbb2
-set -g fish_color_command     b8bb26
-set -g fish_color_keyword     fb4934
-set -g fish_color_quote       b8bb26
-set -g fish_color_redirection d3869b
-set -g fish_color_end         fe8019
-set -g fish_color_error       fb4934
-set -g fish_color_param       ebdbb2
-set -g fish_color_comment     928374
-set -g fish_color_operator    8ec07c
-set -g fish_color_escape      fe8019
-set -g fish_color_autosuggestion 7c6f64
-set -g fish_color_cwd         fabd2f
-set -g fish_color_user        b8bb26
-set -g fish_color_host        83a598
+# Fish colors (use terminal ANSI palette)
+set -g fish_color_normal         normal
+set -g fish_color_command        green
+set -g fish_color_keyword        red
+set -g fish_color_quote          green
+set -g fish_color_redirection    magenta
+set -g fish_color_end            yellow
+set -g fish_color_error          red
+set -g fish_color_param          normal
+set -g fish_color_comment        brblack
+set -g fish_color_operator       cyan
+set -g fish_color_escape         yellow
+set -g fish_color_autosuggestion brblack
+set -g fish_color_cwd            yellow
+set -g fish_color_user           green
+set -g fish_color_host           blue
 
 # Source private config if it exists
 if test -f ~/.private.fish
